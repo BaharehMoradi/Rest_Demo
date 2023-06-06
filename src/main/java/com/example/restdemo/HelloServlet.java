e7f9cd60eccc0f8fa01478bdf1277c8834dc0059
Bahareh Moradi <b.m_89@yahoo.com>
2023-06-06 20:35:40 +0330

    simple rest web service example-v1

---
+++ src/main/java/com/example/restdemo/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.example.restdemo;
+
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet is an annotation to indicate that the Java class should be treated as a servlet and registered with the
+//Servlet container. The name parameter specifies the name of the servlet that will be registered with the container.
+//The value parameter specifies the URL pattern or patterns that the servlet should be mapped to.
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "This is a message for you! You have chosen   /hello-servlet   path.";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Rest World</title>
+</head>
+<body>
+<h1><%= "Hello and welcome to the default landing page!" %>
+</h1>
+<br/>
+<a href="hello-servlet">Enter Servlet</a>
+</body>
+</html>
