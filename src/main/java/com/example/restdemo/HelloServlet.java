47cf675b215190a057c7b6aedb449d9b8eb54653
Bahareh Moradi <b.m_89@yahoo.com>
2023-08-13 10:55:56 +0330

    Adding log4j as a logger to the rest web service

--- src/main/java/com/example/restdemo/HelloServlet.java
+++ src/main/java/com/example/restdemo/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.example.restdemo;
 
 import java.io.*;
+import org.apache.log4j.Logger;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+
+    private static final Logger logger_Main = Logger.getLogger(HelloServlet.class);
+
     @Override
     public void init() {
         message = "This is a message for you! You have chosen   /hello-servlet   path.";
+        logger_Main.info("Method init was invoked.");
     }
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -23,10 +28,14 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        logger_Main.info("Method doGet was invoked.");
+
     }
     @Override
     public void destroy() {
 // Perform any necessary cleanup or resource releasing operations here
         System.out.println("Servlet destroyed");
+        logger_Main.info("Method destroy was invoked.");
+
     }
 }
